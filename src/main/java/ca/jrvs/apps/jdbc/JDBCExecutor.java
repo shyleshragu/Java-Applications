281f63051036fb1498170a340216a335168f2ee0
shyleshragun <shylesh.ragunayakam.jrvs.ca@gmail.com>
2019-07-07 19:40:09 +0000

    updated jdbc so it is understandable when looked at the next time

--- src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -11,24 +11,26 @@ public class JDBCExecutor {
         ApacheDataSource dcm = new ApacheDataSource("localhost", "hplussport", "postgres", "password");
 
         try {
-            Connection connection = dcm.getConnection();
-            //CustomerDAO customerDAO = new CustomerDAO(connection);
-            OrderDAO orderDAO = new OrderDAO(connection);
-
 
             //........create
+            //Connection connection = dcm.getConnection();
+            //CustomerDAO customerDAO = new CustomerDAO(connection);
             //Customer customer = new Customer();
             //customer.setFirstName("George"); customer.setLastName("Washington"); customer.setEmail("george@wh.com"); customer.setPhone("(123) 456-7890");
             //customer.setState("Washington"); customer.setCity("DC"); customer.setZipcode("123321"); customer.setAddress("1 random st");
             //customerDAO.create(customer);
 
             //........read
+            //Connection connection = dcm.getConnection();
+            //CustomerDAO customerDAO = new CustomerDAO(connection);
             //Customer customer = customerDAO.findById(1000);
             //System.out.println(customer.getFirstName() + " " + customer.getLastName());
-            Order order = orderDAO.findById(1000);
-            System.out.println(order);
+            //Order order = orderDAO.findById(1000);
+            //System.out.println(order);
 
             //........update
+            //Connection connection = dcm.getConnection();
+            //CustomerDAO customerDAO = new CustomerDAO(connection);
             //Customer customer = customerDAO.findById(10000);
             //System.out.println(customer.getFirstName()+" "+ customer.getLastName() + " " + customer.getEmail());
             //customer.setEmail("gwash@wh.gov");
@@ -36,6 +38,8 @@ public class JDBCExecutor {
             //System.out.println(customer.getFirstName()+" "+ customer.getLastName() + " " + customer.getEmail());
 
             //.........delete
+            //Connection connection = dcm.getConnection();
+            //CustomerDAO customerDAO = new CustomerDAO(connection);
             //Customer customer = new Customer();
             //customer.setFirstName("Delete"); customer.setLastName("Man"); customer.setEmail("deleteman@wh.com"); customer.setPhone("(123) 010-0000");
             //customer.setState("Atlantis"); customer.setCity("Atlantis"); customer.setZipcode("000000"); customer.setAddress("fallen world");
@@ -49,6 +53,11 @@ public class JDBCExecutor {
             //customerDAO.delete(dbCustomer.getId());
 
 
+            //repository pattern
+            Connection connection = dcm.getConnection();
+            OrderDAO orderDAO = new OrderDAO(connection);
+            Order order = orderDAO.findById(1000);
+            System.out.println(order);
 
         } catch(SQLException e){
             e.printStackTrace();
